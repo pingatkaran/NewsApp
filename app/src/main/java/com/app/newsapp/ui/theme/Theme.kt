254c4cb6592d92ab2f9bcd1a6b11a6d340e8a7c6
Karan Mahboobani <pingatkaran@gmail.com>
2025-08-11 16:06:39 +0530

    added media images

--- app/src/main/java/com/app/newsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/app/newsapp/ui/theme/Theme.kt
@@ -76,7 +76,6 @@ fun NewsAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
